44e0fc31b71ad49b8d9f21046b2d372e7e374175
inbalmishal <inbal.mishal3@gmail.com>
2020-05-30 21:44:39 +0300

    create class person

---
+++ src/Entities/Person.java
@@ -0,0 +1,31 @@
+package Entities;
+
+public class Person {
+    protected String name, address;
+    protected int id, phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
