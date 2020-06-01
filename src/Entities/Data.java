bb4535016b97bc6c90c0b2db04cd1aa3c7b63b10
inbalmishal <inbal.mishal3@gmail.com>
2020-06-01 20:42:29 +0300

    Merge branch 'master' of https://github.com/RafiAriel/ProjectClothingStore
    # Conflicts:
    #	src/Entities/Data.java

--- src/Entities/Data.java
+++ src/Entities/Data.java
@@ -30,7 +30,7 @@ public class Data {
     Connection connection = null;
     try {
         Class.forName("com.mysql.jdbc.Driver").newInstance();
-        connection = DriverManager.getConnection("jdbc:mysql://localhost/db?useSSL=false", "root", "ProjectClothingStore");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/db?useSSL=false", "root", "6560634i");
 
         // Step 2:Create a statement using connection object
         Statement stmt = connection.createStatement();
@@ -70,7 +70,7 @@ public class Data {
             ClubMembers = new ArrayList<Member>();
 
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/db?useSSL=false", "root", "ProjectClothingStore");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/db?useSSL=false", "root", "6560634i");
 
             // Step 2:Create a statement using connection object
             Statement stmt = connection.createStatement();
@@ -112,7 +112,7 @@ public class Data {
 
 
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/db?useSSL=false", "root", "ProjectClothingStore");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/db?useSSL=false", "root", "6560634i");
 
             // Step 2:Create a statement using connection object
             Statement stmt = connection.createStatement();
@@ -163,6 +163,55 @@ public class Data {
         }
 
         return items;
+    }
+
+    public static ArrayList<Worker> getWorkers() {
+        Connection connection = null;
+        ArrayList<Worker> Workers = null;
+        try {
+            Workers = new ArrayList<Worker>();
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/db?useSSL=false", "root", "6560634i");
+
+            // Step 2:Create a statement using connection object
+            Statement stmt = connection.createStatement();
+
+            // Step 3: Execute the query or update query
+            ResultSet rs = stmt.executeQuery("select * from workers");
+            {
+
+                // Step 4: Process the ResultSet object.
+                while (rs.next()) {
+                    String name = rs.getString("name");
+                    int id = rs.getInt("id");
+                    String dateofbirth = rs.getString("dateofbirth");
+                    double hourlysalary = rs.getDouble("hourlysalary");
+                    double numHourMonth = rs.getDouble("numHourMonth");
+                    String jobType = rs.getString("jobType");
+                    String password = rs.getString("password");
+                    Worker w = new Worker(name, dateofbirth, id ,hourlysalary, numHourMonth, jobType, password);
+                    Workers.add(w);
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return Workers;
     }
 }
 
