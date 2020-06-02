81b2ec850cff14d49d4e413ac74ba9e52fb42f1b
Rafi <rafiariel000@gmail.com>
2020-06-02 18:28:43 +0300

    rafi

--- src/Entities/MVCDriver.java
+++ src/Entities/MVCDriver.java
@@ -8,34 +8,17 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import Model.myModel;
-import static java.lang.System.*;
+
 
 public class MVCDriver {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         myModel model = new myModel();
         myCLI view = new myCLI();
-        myController co =  new myController (model , view);
-        ArrayList<Item> items = new ArrayList<>();
-        //ArrayList<Purchase> all = new ArrayList<>();
+        myController co = new myController(model, view);
+        model.deleteClubMember(10);
 
-        Purchase p=  model.lastPurchase(2);
-
-        for(int i=0;i<p.getItem().size();i++) {
-            System.out.print(p.getItem().get(i).getBrand());
-            System.out.print('\n');
-            //    System.out.println(items.get(i).getPrice());
-        }
-       /* all = Data.getInstance().getAllPurchase();
-        for(int i=0;i<all.size();i++) {
-            System.out.print(all.get(i).getPrice());
-            System.out.print(' ');
-        //    System.out.println(items.get(i).getPrice());
-        }*/
-        System.out.print('\n');
-       System.out.print( model.bestSellingProduct().getCurrentStock());
     }
-
 }
 
 
