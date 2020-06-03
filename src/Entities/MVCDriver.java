fc6c7ca0f0519ab4d27d024c9d4397432e1370f2
inbalmishal <inbal.mishal3@gmail.com>
2020-06-03 03:22:17 +0300

    Merge branch 'master' of https://github.com/RafiAriel/ProjectClothingStore

--- src/Entities/MVCDriver.java
+++ src/Entities/MVCDriver.java
@@ -6,24 +6,34 @@ import Model.inbal;
 import View.myCLI;
 import java.io.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import Model.myModel;
+import static java.lang.System.*;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
-
-import Model.myModel;
-
 
 public class MVCDriver {
 
     public static void main(String[] args) {
         myModel model = new myModel();
         myCLI view = new myCLI();
-        myController co = new myController(model, view);
+        myController co =  new myController (model , view);
+        ArrayList<Item> items = new ArrayList<>();
+        inbal inb = new inbal();
 
+        Purchase p =  model.lastPurchase(3);
+        Member m = new Member("rafi", "7/6/2002", 10,2000);
+        Shirt s = new Shirt("blue", "adidas","men","shirt", 300, 10, 300, 600, 1, "wow");
+        Shirt w = new Shirt("blue", "adidas","men","shirt", 300, 10, 300, 600, 2, "wow");
 
-
-
-
+        items.add(s);
+        items.add(w);
+        Purchase a = new Purchase(m, items, 500,100);
+        model.addClubMember(m);
+        System.out.print('\n');
+        System.out.println( inb.Selling(a));
     }
 
 }
