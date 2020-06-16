b19f9e29da1a8f7664924ed1ca22c101fd5d9b7d
orshmar18 <or448623773or@gmail.com>
2020-06-16 15:09:41 +0300

    fix BestSellingProductTest(try)

--- tests/itemControllerTests/BestSellingProductTest.java
+++ tests/itemControllerTests/BestSellingProductTest.java
@@ -1,18 +1,28 @@
 package itemControllerTests;
-
 import controller.ItemController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-
-
-
+import org.junit.jupiter.api.Test;
 
 
 public class BestSellingProductTest {
     ItemController itemController;
 
-
     @BeforeEach
     public void setUp() {itemController = new ItemController();}
 
+    @Test
+    public void BestSellingProductFoundedTest()
+    {
+        Item item = itemController.bestSellingProduct();
+        Assertions.assertNotNull(item);
+    }
+
+    @Test
+    public void BestSellingProductNotFoundSucceedTest()
+    {
+        Item item = itemController.bestSellingProduct();
+        Assertions.assertNull(item == null);
+    }
 
 }
