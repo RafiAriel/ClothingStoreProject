423555a08c4a25a4eecb32ad1d0da9e2883d7af1
RafiAriel <65024803+RafiAriel@users.noreply.github.com>
2020-07-06 23:09:19 +0300

    Update AddShoeTest.java

--- tests/itemControllerTests/AddShoeTest.java
+++ tests/itemControllerTests/AddShoeTest.java
@@ -1,8 +1,6 @@
 package itemControllerTests;
 
 import controller.ItemController;
-import model.entities.Pants;
-import model.entities.Shirt;
 import model.entities.Shoe;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,7 +27,7 @@ public class AddShoeTest {
     @Test
     public void failIdTest() {
         try {
-            Shoe i = new Shoe("blue", "niki", "men", "pants", 200, MOCK_POSITIVE, 200, 500, MOCK_NEGATIVE, "lycra");
+            Shoe i = new Shoe("blue", "niki", "men", "shoe", 200, MOCK_POSITIVE, 200, 500, MOCK_NEGATIVE, "lycra");
             itemController.addShoe(i);
             fail("The addition succeed when should failed");
         } catch (IllegalArgumentException e) {
@@ -42,7 +40,7 @@ public class AddShoeTest {
     @Test
     public void failSizeTest() {
         try {
-            Shoe i = new Shoe("blue", "niki", "men", "pants", 200, MOCK_NEGATIVE, 200, 500, MOCK_POSITIVE, "lycra");
+            Shoe i = new Shoe("blue", "niki", "men", "shoe", 200, MOCK_NEGATIVE, 200, 500, MOCK_POSITIVE, "lycra");
             itemController.addShoe(i);
             fail("The addition succeed when should failed");
         } catch (IllegalArgumentException e) {
@@ -58,8 +56,8 @@ public class AddShoeTest {
     @Test
     public void failAddShoeTest() {
         try {
-            Pants i = new Pants("blue", "niki", "men", "pants", 200, MOCK_SIZE_EXIST, 200, 500, MOCK_ID_EXIST, "bermuda");
-            itemController.addPants(i);
+            Shoe i = new Shoe("blue", "niki", "men", "shoe", 200, MOCK_SIZE_EXIST, 200, 500, MOCK_ID_EXIST, "bermuda");
+            itemController.addShoe(i);
             fail("The addition succeed when should failed");
         } catch (IllegalArgumentException e) {
             Assertions.assertEquals("id and size exist", e.getMessage());
@@ -74,7 +72,7 @@ public class AddShoeTest {
     @Test
     public  void failAddShoeSucceedTest()
     {
-        Shoe i = new Shoe("blue", "niki", "men", "pants", 200, MOCK_POSITIVE, 200, 500, MOCK_ID_NOT_EXIST, "bermuda");
+        Shoe i = new Shoe("blue", "niki", "men", "shoe", 200, MOCK_POSITIVE, 200, 500, MOCK_ID_NOT_EXIST, "bermuda");
         boolean a = itemController.addShoe(i);
         assertTrue(a);
     }
